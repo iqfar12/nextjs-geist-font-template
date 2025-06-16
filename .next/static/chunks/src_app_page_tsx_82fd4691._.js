362d6694461712933e53399314edeb2e26b8f314
iqfar12 <iqfar.silai@gmail.com>
2025-06-16 17:16:04 +0700

    Add .next/static/chunks/src_app_page_tsx_82fd4691._.js 1750068964227

---
+++ .next/static/chunks/src_app_page_tsx_82fd4691._.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_lodash_20634561._.js",
+  "static/chunks/node_modules_recharts_es6_2d303abd._.js",
+  "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_b854acb4._.js",
+  "static/chunks/node_modules_4bbc0664._.js",
+  "static/chunks/src_a3ac7c7e._.js"
+],
+    source: "dynamic"
+});
